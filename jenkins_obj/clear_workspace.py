8d4b175b2c0ec71a5ccef6faeea04d0236782257
TomNewChao <chaotomzhu@gmail.com>
2024-03-07 14:17:32 +0800

    fix bug about clear_workspace

--- jenkins_obj/clear_workspace.py
+++ jenkins_obj/clear_workspace.py
@@ -7,23 +7,29 @@
 import argparse
 import jenkins
 import xmltodict
+from collections import OrderedDict
 
-clean_up_plugin = '''<publishers>
-    <hudson.plugins.ws__cleanup.WsCleanup plugin="ws-cleanup@{}">
-    <patterns class="empty-list"/>
-    <deleteDirs>false</deleteDirs>
-    <skipWhenFailed>false</skipWhenFailed>
-    <cleanWhenSuccess>true</cleanWhenSuccess>
-    <cleanWhenUnstable>true</cleanWhenUnstable>
-    <cleanWhenFailure>true</cleanWhenFailure>
-    <cleanWhenNotBuilt>true</cleanWhenNotBuilt>
-    <cleanWhenAborted>true</cleanWhenAborted>
-    <notFailBuild>false</notFailBuild>
-    <cleanupMatrixParent>false</cleanupMatrixParent>
-    <externalDelete></externalDelete>
-    <disableDeferredWipeout>false</disableDeferredWipeout>
-    </hudson.plugins.ws__cleanup.WsCleanup>
-  </publishers>'''
+
+def get_template_from_clean_up_plugin(version):
+    clean_up_plugin_dict = OrderedDict()
+    plugin_info = OrderedDict()
+    plugin_class = OrderedDict()
+    plugin_info["@plugin"] = "ws-cleanup@{}".format(version)
+    plugin_class["@class"] = "empty-list"
+    plugin_info["patterns"] = plugin_class
+    plugin_info["deleteDirs"] = 'false'
+    plugin_info["skipWhenFailed"] = 'false'
+    plugin_info["cleanWhenSuccess"] = 'true'
+    plugin_info["cleanWhenUnstable"] = 'true'
+    plugin_info["cleanWhenFailure"] = 'true'
+    plugin_info["cleanWhenNotBuilt"] = 'true'
+    plugin_info["cleanWhenAborted"] = 'true'
+    plugin_info["notFailBuild"] = 'false'
+    plugin_info["cleanupMatrixParent"] = 'false'
+    plugin_info["externalDelete"] = None
+    plugin_info["disableDeferredWipeout"] = 'false'
+    clean_up_plugin_dict["hudson.plugins.ws__cleanup.WsCleanup"] = plugin_info
+    return clean_up_plugin_dict
 
 
 class ArgParseTools(object):
@@ -42,7 +48,6 @@ def main():
     jenkins_imp = jenkins.Jenkins(url=args.url, username=args.username, password=args.password)
     jobs = jenkins_imp.get_all_jobs()
     ws_clean_up = jenkins_imp.get_plugin_info("ws-cleanup")
-    clean_up_full_plugin = clean_up_plugin.format(ws_clean_up["version"])
     for job in jobs:
         if job["_class"] == "hudson.model.FreeStyleProject":
             print("start to wipeout job workspace:{}".format(job["fullname"]))
@@ -51,11 +56,17 @@ def main():
             job_config = xmltodict.parse(job_config_str)
             publishers = job_config["project"]["publishers"]
             if publishers is None:
-                new_config = job_config_str.replace(r"<publishers/>", clean_up_full_plugin)
-                jenkins_imp.reconfig_job(job["fullname"], new_config)
-                print("reconfig job success:{}".format(job["fullname"]))
-            job_config_str = jenkins_imp.get_job_config(job["fullname"])
-            print(job_config_str)
+                template = get_template_from_clean_up_plugin(ws_clean_up["version"])
+                job_config["project"]["publishers"] = template
+                job_xml = xmltodict.unparse(job_config, pretty=True)
+                jenkins_imp.reconfig_job(job["fullname"], job_xml)
+                print("job:{} craete the cleanup config".format(job["fullname"]))
+            elif "hudson.plugins.ws__cleanup.WsCleanup" not in publishers.keys():
+                template = get_template_from_clean_up_plugin(ws_clean_up["version"])
+                job_config["project"]["publishers"].update(template)
+                job_xml = xmltodict.unparse(job_config, pretty=True)
+                jenkins_imp.reconfig_job(job["fullname"], job_xml)
+                print("job:{} add the cleanup config".format(job["fullname"]))
 
 
 if __name__ == '__main__':
